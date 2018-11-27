971fc8603c0d5e01090b2677de72316e69c23f74
Casey Justus <cjustus@keyholesoftware.com>
2018-11-27 14:48:58 -0600

    fixed npm security vulnerabilities

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/css/bootstrap-theme.css'
-import 'font-awesome/css/font-awesome.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/font-awesome/css/font-awesome.min.css'
 import './index.css'
 
 const element = <App />
